07662edbd21fdeff88dc4469891ec42497f2c565
rail <rail.shabayev@mail.ru>
2024-02-24 12:46:34 +0400

    feat: add laba

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+# Вариант 27.
+# Шеснадцатиричные четные числа, не превышающие 2048 и
+# содержащие количество цифр большее, чем вторая цифра числа.
+# Вывести числа и их количество. Максимальное число вывести прописью.
+import os
+import sys
+import num2words
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+desiredDigits = []  # переменная которая будет содержать подходящие по условиям числа
+quantityOfDigits = 0  # переменная которая будет содержать общее количество подходящих условиям чисел
+even = ("0", "2", "4", "6", "8", "A", "C", "E")  # четные числа в 16-ой системе счисления
+file = open("text.txt", "r")
+if os.stat("text.txt").st_size == 0:
+    print("файл является пустым")
+    sys.exit()
+for i in file.readline().split():  # цикл проходящий через числа файла
+    try:
+        a = int(i, 16)  # переводит число из 16-ой в 10-ую систему счисления
+    except ValueError as error:
+        print(error)
+        print("Число записано не в 16-ой системе счисления")
+    if a < 2048:  # проверяет является ли число меньше 2048
+        secondDigit = int(i[1], 16)  # содержит вторую цифру числа переведя его из 16 в 10 систему счисления
+        quantityOfDigitsInNumber = len(str(int(i, 16)))  # содержит общее количество цифр числа переведя его из 16 в 10
+        if quantityOfDigitsInNumber < secondDigit:  # проверяет является ли общее количество цифр цисла больше
+            # его второй цифры
+            if i[-1] in even:  # проверяет является ли число четным
+                desiredDigits.append(i)  # числа удовлетворяющиt условиям
+                quantityOfDigits += 1  # счетчик количества чисел
+if quantityOfDigits != 0:
+    print("цифры походящие по условиям: " + str(desiredDigits))
+    print("количство цифр подходящих по условиям: " + str(quantityOfDigits))
+    max_number = max(desiredDigits)
+    max_number_in_russian = num2words.num2words(int(max_number, 16), lang='ru')
+    print("максимальное число из тех чисел, которые подходят по условиям: " + max_number_in_russian)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+else:
+    print("чисел удовлетворяющих условиям нету")
