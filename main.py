6fe3f71ea4b70a71990389b6294a7e8ce1fc25b7
rail <rail.shabayev@mail.ru>
2024-03-16 13:13:45 +0400

    fix: made laba2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# Лабораторная №2. Регулярные выражения
 # Вариант 27.
 # Шеснадцатиричные четные числа, не превышающие 2048 и
 # содержащие количество цифр большее, чем вторая цифра числа.
 # Вывести числа и их количество. Максимальное число вывести прописью.
 import os
+import re
 import sys
 
 number_dict = {'0': 'ноль',
@@ -23,31 +25,29 @@ number_dict = {'0': 'ноль',
                'F': 'F'}
 desiredDigits = []
 quantityOfDigits = 0
-even = ("0", "2", "4", "6", "8", "A", "C", "E")
 file = open("text.txt", "r")
-if os.stat("text.txt").st_size == 0:
+size = os.stat("text.txt").st_size
+if re.match("0", str(size)):
     print("файл является пустым")
     sys.exit()
 for i in file.readline().split():
-    if i[0] == "0":
+    if re.match("^0", i[0]):
         print("Число " + i + " начинается с 0")
         continue
-    try:
-        a = int(i, 16)
-    except ValueError as error:
-        print(error)
+    if re.match(r'(0x)?[A-Fa-f0-9]+', i) is False:
         print("Число записано не в 16-ой системе счисления")
-    if a < 2048:
-        secondDigit = i[1]
-        quantityOfDigitsInNumber = str(len(i))
-        if quantityOfDigitsInNumber > secondDigit:
-            if i[-1].upper() in even:
+    if int(i, 16) < 2048:
+        sec = i[1]
+        qty = str(len(i))
+        if (re.match("[012]", str(sec)) and re.match("3", qty) or
+                re.match("[012]", str(sec)) and re.match("3", qty)):
+            if re.match("^[0-9A-Fa-f]*[24680ACE]$", i):
                 desiredDigits.append(i)
                 quantityOfDigits += 1
             else:
-                print("16-ое число: " + str(i) + " - kисло не четное")
+                print("16-ое число: " + str(i) + " - число не четное")
         else:
-            print("16-ое число: " + str(i) + " - kоличество цифр меньше второго числа")
+            print("16-ое число: " + str(i) + " - количество цифр меньше второго числа")
     else:
         print("16-ое число: " + str(i) + " - больше 2048 в десятичной системе")
 
